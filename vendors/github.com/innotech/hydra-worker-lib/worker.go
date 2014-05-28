cd2c9de8ac1074ba5188f066ff392833a7295f8b
EC2 Default User <german.ramos@gmail.com>
2014-05-28 16:03:27 +0000

    Fix gozmq dependency path

--- vendors/github.com/innotech/hydra-worker-lib/worker.go
+++ vendors/github.com/innotech/hydra-worker-lib/worker.go
@@ -2,7 +2,7 @@ package worker
 
 import (
 	"encoding/json"
-	zmq "github.com/innotech/hydra/vendors/github.com/alecthomas/gozmq"
+	zmq "github.com/innotech/hydra-worker-sort-by-number/vendors/github.com/innotech/hydra-worker-lib/vendors/github.com/alecthomas/gozmq"
 	"log"
 	"reflect"
 	"time"
